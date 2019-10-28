e0dc0786079650c1ce98631b28716b12b5e5baab
Denis <dietimemovedme@gmail.com>
2019-10-28 16:23:16 +0300

    fixed a few bugs

--- src/screens/TimeTable.js
+++ src/screens/TimeTable.js
@@ -38,7 +38,7 @@ class TimeTable extends Component {
     componentDidMount() {
         fetch('https://cors-anywhere.herokuapp.com/https://mai.ru/education/schedule/detail.php?group=' + this.props.store.group)
             .then((response) => response.text())
-            .then((html)=>{
+            .then((html) => {
                 let parser = new DOMParser();
                 let htmlDoc = parser.parseFromString(html, 'text/html');
                 let cells = htmlDoc.getElementsByClassName('table');
@@ -58,13 +58,12 @@ class TimeTable extends Component {
                     let data = oneDay[0].childNodes[0].data;
                     let subjects = oneDay[1].children[0].children;
                     let dataObj = [];
-                    for (let subject of subjects )
-                    {
+                    for (let subject of subjects) {
                         let subjectObj = {
                             time: subject.children[0].innerText,
                             type: subject.children[2].innerText,
                             title: subject.children[3].children[0].children[0].innerText,
-                            teacher: (subject.children[3].children[0].children.length < 2) ? '' :  subject.children[3].children[0].children[2].innerText,
+                            teacher: (subject.children[3].children[0].children.length < 2) ? '' : subject.children[3].children[0].children[2].innerText,
                             location: subject.children[4].childNodes[1].nodeValue,
                         };
                         dataObj.push(subjectObj);
@@ -77,7 +76,7 @@ class TimeTable extends Component {
                     dataSet.push(dayObj);
                 }
                 this.setState({dataSet: dataSet});
-        })
+            })
     }
 
     getTimeTable = () => {
@@ -85,7 +84,7 @@ class TimeTable extends Component {
         let week = (this.state.currentWeek.text === 'Текущая неделя') ? '' : '&week=' + this.state.currentWeek.index;
         fetch('https://cors-anywhere.herokuapp.com/https://mai.ru/education/schedule/detail.php?group=' + this.props.store.group + week)
             .then((response) => response.text())
-            .then((html)=>{
+            .then((html) => {
                 let parser = new DOMParser();
                 let htmlDoc = parser.parseFromString(html, 'text/html');
                 let cells = htmlDoc.getElementsByClassName('sc-table-day');
@@ -96,13 +95,12 @@ class TimeTable extends Component {
                     let data = oneDay[0].childNodes[0].data;
                     let subjects = oneDay[1].children[0].children;
                     let dataObj = [];
-                    for (let subject of subjects )
-                    {
+                    for (let subject of subjects) {
                         let subjectObj = {
                             time: subject.children[0].innerText,
                             type: subject.children[2].innerText,
                             title: subject.children[3].children[0].children[0].innerText,
-                            teacher: (subject.children[3].children[0].children.length < 2) ? '' :  subject.children[3].children[0].children[2].innerText,
+                            teacher: (subject.children[3].children[0].children.length < 2) ? '' : subject.children[3].children[0].children[2].innerText,
                             location: subject.children[4].childNodes[1].nodeValue,
                         };
                         dataObj.push(subjectObj);
@@ -168,9 +166,9 @@ class TimeTable extends Component {
                                         this.getTimeTable();
                                         this.modalBack();
                                     }} key={text}>{
-                                        (index !== 0 )
-                                        ? this.dateReformat(text) + ' (Неделя ' + index + ')'
-                                        : text }
+                                        (index !== 0)
+                                            ? this.dateReformat(text) + ' (Неделя ' + index + ')'
+                                            : text}
                                     </Radio>
                                 );
                             })}
@@ -180,13 +178,12 @@ class TimeTable extends Component {
             </ModalRoot>
         );
 
-        if (this.state.weeks.length === 0){
+        if (this.state.weeks.length === 0) {
             return (
                 <Spinner size="large"
                          style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, margin: 'auto'}}/>
             )
-        }
-        else if (this.state.dataSet.length === 0){
+        } else if (this.state.dataSet.length === 0) {
             return (
                 <View id='main' activePanel='main' modal={modal}>
                     <Panel id='main'>
@@ -205,8 +202,7 @@ class TimeTable extends Component {
                     </Panel>
                 </View>
             )
-        }
-        else {
+        } else {
             return (
                 <View id='main' activePanel='main' modal={modal}>
                     <Panel id='main'>
@@ -220,46 +216,109 @@ class TimeTable extends Component {
                                 {this.props.store.group}
                             </PanelHeaderContent>
                         </PanelHeader>
-                            {this.state.dataSet.map((item, index)=>{
-                                return (
-                                    <Div key={index} style={(index !== 0) ? {paddingTop: 0, paddingBottom: 14} : {paddingTop: 14, paddingBottom: 14}}>
-                                        <Div style={{padding: 0}}>
-                                            <Header level="secondary" style={{background: '#00a1f5', color: '#fff', height: 'auto', paddingRight: 20, paddingLeft: 20}} aside={<h3 style={{margin: 0, color: '#fff'}}>{item.day}</h3>}>
-                                                <h3 style={{margin: 0, color: '#fff'}}>{item.data}</h3>
-                                            </Header>
-                                        </Div>
-                                        {item.subjects.map((subject, index) => {
-                                            return (
-                                                <Div key={index} style={(index === 0) ? {background: '#fff', paddingLeft: 20, paddingRight: 20, paddingTop: 20, paddingBottom: 0} : {background: '#fff', paddingLeft: 20, paddingRight: 20, paddingTop: 0, paddingBottom: 0}}>
-                                                    {(index !== 0) ? <div style={{marginBottom: 20, height: '1px', background: '#dcdcdc'}}/> : null}
-                                                    <Div style={{alignItems: 'center', justifyContent: 'space-between', display: 'flex', padding: 0}}>
-                                                        <Div style={{fontSize: 19, color: '#323232', fontWeight: 700, padding: 0}}>
-                                                            {subject.time}
-                                                        </Div>
-                                                        <p style={{fontSize: 15, color: '#323232', margin: 0, fontWeight: 500}}>
-                                                            {subject.type}
-                                                        </p>
-                                                    </Div>
-                                                    <Div style={{fontSize: 18, color: '#323232', fontWeight: 600, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, paddingTop: 10}}>
-                                                        {subject.title}
-                                                    </Div>
-                                                    {(subject.teacher !== '') ?
-                                                        <Div style={{fontSize: 13, color: '#323232', paddingLeft: 0, paddingRight: 0, paddingBottom: 0, paddingTop: 10}}>
-                                                            {subject.teacher}
-                                                        </Div>
-                                                        :
-                                                        null
-                                                    }
-                                                    <Div style={{fontSize: 14, color: '#323232', fontWeight: 600, paddingLeft: 0, paddingRight: 0, paddingBottom: 20, paddingTop: 10}}>
-                                                        {subject.location}
-                                                    </Div>
-                                                </Div>
-                                            )
-                                        })
-                                    }
+                        {this.state.dataSet.map((item, index) => {
+                            return (
+                                <Div key={index} style={(index !== 0) ? {paddingTop: 0, paddingBottom: 14} : {
+                                    paddingTop: 14,
+                                    paddingBottom: 14
+                                }}>
+                                    <Div style={{padding: 0}}>
+                                        <Header level="secondary" style={{
+                                            background: '#00a1f5',
+                                            color: '#fff',
+                                            height: 'auto',
+                                            paddingRight: 20,
+                                            paddingLeft: 20
+                                        }} aside={<h3 style={{margin: 0, color: '#fff'}}>{item.day}</h3>}>
+                                            <h3 style={{margin: 0, color: '#fff'}}>{item.data}</h3>
+                                        </Header>
                                     </Div>
-                                )
-                            })}
+                                    {item.subjects.map((subject, index) => {
+                                        return (
+                                            <Div key={index} style={(index === 0) ? {
+                                                background: '#fff',
+                                                paddingLeft: 20,
+                                                paddingRight: 20,
+                                                paddingTop: 20,
+                                                paddingBottom: 0
+                                            } : {
+                                                background: '#fff',
+                                                paddingLeft: 20,
+                                                paddingRight: 20,
+                                                paddingTop: 0,
+                                                paddingBottom: 0
+                                            }}>
+                                                {(index !== 0) ? <div style={{
+                                                    marginBottom: 20,
+                                                    height: '1px',
+                                                    background: '#dcdcdc'
+                                                }}/> : null}
+                                                <Div style={{
+                                                    alignItems: 'center',
+                                                    justifyContent: 'space-between',
+                                                    display: 'flex',
+                                                    padding: 0
+                                                }}>
+                                                    <Div style={{
+                                                        fontSize: 19,
+                                                        color: '#323232',
+                                                        fontWeight: 700,
+                                                        padding: 0
+                                                    }}>
+                                                        {subject.time}
+                                                    </Div>
+                                                    <p style={{
+                                                        fontSize: 15,
+                                                        color: '#323232',
+                                                        margin: 0,
+                                                        fontWeight: 500
+                                                    }}>
+                                                        {subject.type}
+                                                    </p>
+                                                </Div>
+                                                <Div style={{
+                                                    fontSize: 18,
+                                                    color: '#323232',
+                                                    fontWeight: 600,
+                                                    paddingLeft: 0,
+                                                    paddingRight: 0,
+                                                    paddingBottom: 0,
+                                                    paddingTop: 10
+                                                }}>
+                                                    {subject.title}
+                                                </Div>
+                                                {(subject.teacher !== '') ?
+                                                    <Div style={{
+                                                        fontSize: 13,
+                                                        color: '#323232',
+                                                        paddingLeft: 0,
+                                                        paddingRight: 0,
+                                                        paddingBottom: 0,
+                                                        paddingTop: 10
+                                                    }}>
+                                                        {subject.teacher}
+                                                    </Div>
+                                                    :
+                                                    null
+                                                }
+                                                <Div style={{
+                                                    fontSize: 14,
+                                                    color: '#323232',
+                                                    fontWeight: 600,
+                                                    paddingLeft: 0,
+                                                    paddingRight: 0,
+                                                    paddingBottom: 20,
+                                                    paddingTop: 10
+                                                }}>
+                                                    {subject.location}
+                                                </Div>
+                                            </Div>
+                                        )
+                                    })
+                                    }
+                                </Div>
+                            )
+                        })}
                     </Panel>
                 </View>
             )
